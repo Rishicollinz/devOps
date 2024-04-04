23df314a174ef021fa043f8b128573766374a9b6
rishicollinz <rishikesh.b@codingmart.com>
2024-04-04 23:15:01 +0530

    cdn added to notes

--- pm2/ex1/ecosystem.config.js
+++ pm2/ex1/ecosystem.config.js
@@ -4,9 +4,14 @@ module.exports = {
     script: "npm",
     args: "run dev", // Assuming your start script for Vite is 'npm run dev'
     cwd: "/home/rishikeshb/Documents/DevOps/pm2/ex1", // Change this to the actual path of your Vite app
-    watch: true,
+    watch: false,
     ignore_watch: ["node_modules"],
-    instances: "4",
-    exec_mode: "cluster"
+    instances: "8",
+    exec_mode: "cluster",
+    autorestart: true,
+    max_memory_restart: '100M',
+    restart_delay: 5000, // Optional: Delay between restarts (in milliseconds)
+    // Specify restart strategy
+    restart: 'always' // or 'never' or 'on-failure'
   }]
 };
